cb5a8ca4f302184b76295a9d84788bde9629d6b4
Megan Baig <git@megs.bulldy80.com>
2015-07-12 12:25:02 -0400

    added quitter

--- sum_game.py
+++ sum_game.py
@@ -4,17 +4,24 @@ import sys
 def sum_game():
     num1 = random.randint(1,101)
     num2 = random.randint(1,101)
+    num_enters = 0
     while True:
         print('%s + %s = ??' % (num1,num2))
         print('What\'s the sum?')
         ans = input()
         if ans.strip() == "":
-            print('C\'mon.. you can do this!')
+            num_enters = num_enters + 1
+            if num_enters == 5:
+                print('Quitter...')
+                print('')
+                return  #takes you out of the function
+            else:
+                print('C\'mon.. you can do this!')
         else:
             i = int(ans)
             if i == num1+num2:
                 print('Yay! You\'re smaht!')
-                break
+                break   #takes you out of the loop
             elif i != num1+num2:
                 print('Nope! Go back to school!')
 
@@ -22,7 +29,7 @@ def again():
     while True:
         print('Press enter to play again')
         input()
-        sum_game()
+        sum_game()    
 
 sum_game()
 again()
