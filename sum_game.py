fb21a97fa63f084081494dd78239f3db76943537
Megan Baig <git@megs.bulldy80.com>
2015-07-12 12:51:55 -0400

    added more words of encouragement (global variable)

--- sum_game.py
+++ sum_game.py
@@ -1,10 +1,14 @@
 import random
 import sys
 
+right_ans = 0
+
 def sum_game():
     num1 = random.randint(1,101)
     num2 = random.randint(1,101)
+    global right_ans  #use global variable (don't create a local variable)
     num_enters = 0
+    wrong_ans = 0
     while True:
         print('%s + %s = ??' % (num1,num2))
         print('What\'s the sum?')
@@ -20,9 +24,17 @@ def sum_game():
         else:
             i = int(ans)
             if i == num1+num2:
+                right_ans = right_ans + 1
+                if right_ans == 5:
+                    print('Wooo! You\'re on a roll!')
+                    return
                 print('Yay! You\'re smaht!')
                 break   #takes you out of the loop
             elif i != num1+num2:
+                wrong_ans = wrong_ans + 1
+                if wrong_ans == 5:
+                    print('You really need to go back to school..')
+                    return
                 print('Nope! Go back to school!')
 
 def again():
